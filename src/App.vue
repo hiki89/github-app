b18dfaf98ed48eb66b68fe68b6e1bd3b8426afab
hiki89 <markohitl89@gmail.com>
2018-09-06 22:02:24 +0200

    Zadatak 6

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
       <AppToolbar />
       <div class="center">
         <app-search :query.sync="query"/>
+        <v-ons-list>
+          <v-ons-list-header>{{`Repositories of ${this.query}`}}</v-ons-list-header>
+          <v-ons-list-item v-for="repo in repos" :key="repo.id">
+            <div class="left">
+              <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
+            </div>
+            <div class="center">
+              <span class="list-item__title">{{repo.name}}</span>
+              <span class="list-subtitle">{{repo.description}}</span>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
       </div>
     </v-ons-page>
 </template>
