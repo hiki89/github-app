e4f612dff29ab96d7427a2cb995f371c76875959
hiki89 <markohitl89@gmail.com>
2018-09-06 19:28:04 +0200

    Zadatak 2

---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="left">
+
+            </div>
+            <div class="center">
+                {{title}}
+            </div>
+            <div class="right">
+
+            </div>
+        </v-ons-toolbar>
+    </v-ons-page>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'Github'
+        }
+    }
+}
+</script>
+
