3b5bf75e0e494e8bb57e5b16974ee75cb39cb194
hiki89 <markohitl89@gmail.com>
2018-09-07 21:02:49 +0200

    Zadatak 10

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,16 @@
 <template>
-    <v-ons-page>
-      <AppToolbar />
-      <div class="center">
-        <app-search :query.sync="query"/>
-        <v-ons-list>
-          <v-ons-list-header>{{`Repositories of ${this.query}`}}</v-ons-list-header>
-          <div v-if="this.loadingRepos">
-          <v-ons-progress-circular indeterminate></v-ons-progress-circular>
-          </div>
-          
-            <empty-state v-if="this.repos.length === 0" type="repository"/>
-            <error404 v-if="this.err" />
-          
-          <v-ons-list-item v-for="repo in repos" :key="repo.id">
-            <div class="left">
-              <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
-            </div>
-            <div class="center">
-              <span class="list-item__title">{{repo.name}}</span>
-              <span class="list-item__subtitle">{{repo.description}}</span>
-            </div>
-          </v-ons-list-item>
-        </v-ons-list>
-      </div>
-    </v-ons-page>
+    <v-ons-navigator :page-stack="pageStack"></v-ons-navigator>
 </template>
 <script>
-import debounce from 'lodash/debounce'
-
-import AppToolbar from './components/AppToolbar'
-import AppSearch from './components/AppSearch'
-import EmptyState from './components/EmptyState'
-import error404 from './components/404'
-import {gitHub} from './services/GitHub'
+import Repositories from './components/Repositories'
  
 export default {
-  components: {
-    AppToolbar,
-    AppSearch,
-    EmptyState,
-    error404
-  },
-
   data() {
     return {
-      query: '',
-      repos: [],
-      loadingRepos: false,
-      err: false
+      pageStack: [ Repositories ]
     }
   },
 
-  watch: {
-    query: debounce (function (newValue) {
-      this.loadingRepos = true
-      gitHub.getRepos(newValue)
-      .then((response) => {
-        this.repos = response.data
-        console.log(this.repos)
-      })
-      .catch(() => {
-        this.err = true
-      })
-      .finally(() => {
-        this.loadingRepos = false
-      })
-    }, 500)
-  },
-
-
-
-  methods: {
-    // repo(username) {
-    //   GitHub.getRepos(username)
-    //   .then(response => {
-    //     next(repos => {
-    //       this.repos = response.data
-    //       console.log(this.repos)
-    //     })
-        
-    //   })
-      
-    // }
-    
-  }
 };
 </script>
 
---
+++ src/components/Repositories.vue
@@ -0,0 +1,93 @@
+<template>
+    <v-ons-page>
+      <AppToolbar />
+      <div class="center">
+        <app-search :query.sync="query"/>
+        <div>
+        <v-ons-button>View Profile</v-ons-button>
+        </div>
+        <v-ons-list>
+          <v-ons-list-header>{{`Repositories of ${this.query}`}}</v-ons-list-header>
+          <div v-if="this.loadingRepos">
+          <v-ons-progress-circular indeterminate></v-ons-progress-circular>
+          </div>
+          
+            <empty-state v-if="this.repos.length === 0" type="repository"/>
+            <error404 v-if="this.err" />
+          
+          <v-ons-list-item v-for="repo in repos" :key="repo.id">
+            <div class="left">
+              <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
+            </div>
+            <div class="center">
+              <span class="list-item__title">{{repo.name}}</span>
+              <span class="list-item__subtitle">{{repo.description}}</span>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
+      </div>
+    </v-ons-page>
+</template>
+<script>
+import debounce from 'lodash/debounce'
+
+import AppToolbar from './AppToolbar'
+import AppSearch from './AppSearch'
+import EmptyState from './EmptyState'
+import error404 from './404'
+import {gitHub} from './../services/GitHub'
+ 
+export default {
+  components: {
+    AppToolbar,
+    AppSearch,
+    EmptyState,
+    error404
+  },
+
+  data() {
+    return {
+      query: '',
+      repos: [],
+      loadingRepos: false,
+      err: false
+    }
+  },
+
+  watch: {
+    query: debounce (function (newValue) {
+      this.loadingRepos = true
+      gitHub.getRepos(newValue)
+      .then((response) => {
+        this.repos = response.data
+        console.log(this.repos)
+      })
+      .catch(error => {
+        if(error.response.status === 404) {
+          this.err =true
+        }
+      })
+      .finally(() => {
+        this.loadingRepos = false
+      })
+    }, 500)
+  },
+
+
+
+  methods: {
+    // repo(username) {
+    //   GitHub.getRepos(username)
+    //   .then(response => {
+    //     next(repos => {
+    //       this.repos = response.data
+    //       console.log(this.repos)
+    //     })
+        
+    //   })
+      
+    // }
+    
+  }
+};
+</script>
